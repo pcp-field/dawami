42deae7d393c3b82ce092a399321389e942e7790
pcp-field <ialbakri99@gmail.com>
2026-08-29 02:30:51 +0400

    تحديث كاش التطبيق للإصدار 4.2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="dawami-simple-v4-1";
-const ASSETS=["./","./index.html","./simple.css?v=4.1","./core.js?v=4.1","./simple.js?v=4.1","./manifest.webmanifest?v=4.1","./icon.svg"];
+const CACHE="dawami-simple-v4-2";
+const ASSETS=["./","./index.html","./simple.css?v=4.2","./core.js?v=4.2","./simple.js?v=4.2","./manifest.webmanifest?v=4.2","./icon.svg"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
