485a1ff94baf0dfdd7e70a67f8083eba228c5a75
pcp-field <ialbakri99@gmail.com>
2026-08-29 12:49:10 +0400

    تحويل التقويم والإعدادات إلى شاشات منبثقة للهاتف

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="dawami-v7-1";
-const ASSETS=["./","./index.html","./dawami-v7.css?v=7.0","./core.js?v=7.0","./dawami-v7-1.js?v=7.1","./manifest.webmanifest?v=7.0","./icon.svg?v=7.0","./holidays-om.json"];
+const CACHE="dawami-v8-0";
+const ASSETS=["./","./index.html","./dawami-v8.css?v=8.0","./core.js?v=8.0","./dawami-v8.js?v=8.0","./manifest.webmanifest?v=8.0","./icon.svg?v=8.0","./holidays-om.json"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener("fetch",e=>{
